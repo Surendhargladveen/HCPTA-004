7d769fd786ae8e3f2844d2be2eaca07330cee783
Surendhar A <surendharfx@gmail.com>
2026-02-14 22:40:36 +0530

    HCPTA-004

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "port" {
+    description = "The port on which the security group will allow traffic"
+    type        = number
+    default     = 443
+}
+
+variable "cidrblock" {
+   description = "CIDR block"
+   type = string
+   default = "10.0.0.0/16"
+}
