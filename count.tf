92d80e648cf707479dfe89efc85c3858e2091a1a
Surendhar A <surendharfx@gmail.com>
2026-02-15 13:12:33 +0530

    HCPTA-004

---
+++ count.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "linux-vm" {
+    ami           = "ami-0b6c6ebed2801a5cb"
+    instance_type = "t2.micro"
+    key_name = "terraform"
+    count = 6
+    
+    tags = {
+        Name = "MyLinuxVM"
+    }
+}
